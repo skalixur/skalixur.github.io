d1c90a023bc13028a881944054c0045574650788
skalixur <juanmateod04@gmail.com>
2023-07-07 02:08:38 +0800

    make easier

--- js/script.js
+++ js/script.js
@@ -1,10 +1,24 @@
+const footerItems = document.querySelectorAll('.footer-item')
 const discordButton = document.querySelector('.footer-discord')
 
 discordButton.addEventListener('click', e => {
   e.preventDefault()
-  discordButton.innerHTML = 'Skalixur#2719 copied!'
+  discordButton.innerHTML = 'copied!'
   setTimeout(() => {
     discordButton.innerHTML = 'discord'
   }, 1000)
-  navigator.clipboard.writeText('Skalixur#2719')
+  navigator.clipboard.writeText('skalixur')
 })
+
+let itemCount = 0
+let footerInterval = setInterval(() => {
+  console.log(footerItems)
+  if (!(itemCount < footerItems.length)) return
+
+  footerItems[itemCount].classList.toggle('footer-animation', true)
+
+  itemCount++
+}, 500)
+setTimeout(() => {
+  clearInterval(footerInterval)
+}, 500 * footerItems.length)
